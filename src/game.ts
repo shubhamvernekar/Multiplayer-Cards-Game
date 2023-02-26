a48ac37da186d6abb56ecb7d5152d4ec7b90beac
Shubham Vernekar <shubhamvernekar@192.168.1.2>
2023-02-26 23:16:14 +0530

    multiclient connection

--- src/game.ts
+++ src/game.ts
@@ -1,15 +1,64 @@
 const ws = require('ws');
 
+class Client {
+    private id : number;
+    private betAmount: number;
+
+    constructor(id: number) {
+        this.id = id;
+        this.betAmount = 0;
+    }
+
+    public addBet(amount: number) {
+        this.betAmount += amount;
+    }
+
+    public getBetAmount() {
+        return this.betAmount;
+    }
+}
+
 export class Game {
-    private playersMap: {[key: number]: string} = {};
+    private clients: Array<Client> = [];
     private wss: any = new ws.Server({port: 3080});
+    private roomCapicity: number = 78;
 
     constructor() {
         this.wss.on('connection', (ws: any) => {
-            console.log('New client connected!'); 
+            let id = this.addNewClient();
+            if(id != -1) {
+                console.log("new client connected");
+                ws.send(`CE:${id}`);
+            } else {
+                ws.terminate();
+            }
+            //send connection established message
+
+            ws.on("close", () => {
+                console.log("Client disconnected");
+            });
+
+            ws.on("message", (message: string) => {
+                this.processMessage(message.toString());
+            }); 
         });
     }
 
-    public joinGame(id: number) {
+    private addNewClient() :  number {
+        if(this.clients.length < this.roomCapicity) {
+            let id : number = this.clients.length+1;
+            this.clients.push(new Client(id));
+            return id;
+        }
+        return -1;
+    }
+
+    private processMessage(message: string) {
+        let prefix : string = message.split(":")[0];
+        let value : string = message.split(":")[1];
+
+        switch(prefix) {
+            case "AT": console.log(`Bet amount value is ${value}`);
+        }
     }
 }
